caf99ec5ebd824bb56af26715f6486c7078073b6
unknown <adetolaadedeji0@gmail.com>
2022-06-24 14:40:59 +0100

    Clean code

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 //get type of operation
 const operator = prompt("What arithmetic opertion Would you like to perform? \n Type A for Addition \n Type S for Subtraction \n Type D for Division \n Type M for Multiplication \n Type Mod for Modulus (%) \n")
 
-
-
-
 //get two input values
-
 const input1 = prompt("Enter value 1: ")
 const input2 = prompt("Enter value 2: ")  
 
 
 //check if operation type is correct
-
 if (operator == "A" || operator == 'a'){
     alert(`${input1} + ${input2} = ${(parseFloat(input1) + parseFloat(input2)).toFixed(2)}`)
 } else if (operator == "S" || operator == 's') {
@@ -23,6 +18,3 @@ if (operator == "A" || operator == 'a'){
 } else if (operator == "Mod" || operator == 'mod') {
     alert(`${input1} % ${input2} = ${(parseFloat(input1) % parseFloat(input2)).toFixed(2)}`)
 }
-//check if numbers are correct
-
-//print out results
